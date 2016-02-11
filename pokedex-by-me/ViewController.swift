f6f0663f6cc5ae551ae15b4d1a65850173dd018c
Al-Rashaa <rahmaalrashdi@host260.kmd.keio.ac.jp>
2016-02-11 20:01:05 +0900

    Initial Commit

---
+++ pokedex-by-me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-me
+//
+//  Created by Rahma Al Rashdi on 2/11/16.
+//  Copyright © 2016 Creato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
